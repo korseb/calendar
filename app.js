b343acab0b49cf5363a839ecf788a2956aa0db5f
Sebastian Korczak <132987126+korseb@users.noreply.github.com>
2023-06-19 13:44:23 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const monthNameEl = document.getElementById("month-name");
+const dayNameEl = document.getElementById("day-name");
+const dayNumEl = document.getElementById("day-number");
+const yearEl = document.getElementById("year");
+
+const date = new Date();
+const month = date.getMonth();
+monthNameEl.innerText = date.toLocaleString("pl", {
+  month: "long",
+});
+
+dayNameEl.innerText = date.toLocaleString("pl", {
+  weekday: "long",
+});
+
+dayNumEl.innerText = date.getDate();
+
+yearEl.innerText = date.getFullYear();
